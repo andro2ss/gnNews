078d5023b933a312ce9cc2fa555a8cadc579125f
andro2ss <andro2ss@gmail.com>
2023-03-30 21:03:18 +0200

    added firebase deploy

--- src/translations/_i18n/i18n.ts
+++ src/translations/_i18n/i18n.ts
@@ -2,13 +2,11 @@ import i18next from "i18next";
 import { i18nResources } from "./i18nResources";
 import langHelper from "../../helpers/langHelper";
 
-// eslint-disable-next-line jest/require-hook
 i18next
   .init({
     interpolation: { escapeValue: false },
     lng: langHelper.currentLanguage(),
     resources: i18nResources,
-    // eslint-disable-next-line no-console
   })
   .catch(console.error);
 
